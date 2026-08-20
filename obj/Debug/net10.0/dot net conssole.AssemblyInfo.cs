9f04850ffbf8b6ec914aa44fbbb56be0dab2280f
Azwer <huzefaazwer51@gmail.com>
2026-08-20 17:42:23 +0500

    Refactor converter into classes with encapsulated state

--- obj/Debug/net10.0/dot net conssole.AssemblyInfo.cs
+++ obj/Debug/net10.0/dot net conssole.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dot net conssole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeab1ff18dfc72b4cb46d90c15301fa0bc42f2bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8811736a4feae6600ce14a7ef4bde028a6457e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("dot net conssole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dot net conssole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
